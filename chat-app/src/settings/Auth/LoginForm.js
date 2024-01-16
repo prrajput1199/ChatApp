c64a6fb6982c4a44926209e377cc1789aaaedf76
Pratik Rajput <pratikrajput1199@gmail.com>
2024-01-16 14:37:53 +0530

    push loginform code

--- chat-app/src/settings/Auth/LoginForm.js
+++ chat-app/src/settings/Auth/LoginForm.js
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, { useState } from "react";
+import FormProvider from "../../components/hook-form/FormProvider";
+import * as Yup from "yup";
+import useForm from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 const Loginform = () => {
+  const [showPassword, setShowPassoword] = useState(false);
+
+  const LoginSchema = Yup.object().shape({
+    email: Yup.string()
+      .required("Email is required")
+      .email("Email must be a valid email address"),
+    password: Yup.string().required("Password is correct"),
+  });
+
+  const defaultvalues = {
+    email: "demo@chat.com",
+    password: "chat1234",
+  };
+
+  const methods = useForm({
+    resolver: yupResolver(Loginform),
+    defaultvalues,
+  });
   return (
-    <div>
-      Login
-    </div>
-  )
-}
+    <>
+      <FormProvider
+        methods={methods}
+        onSubmit={handleSubmit(onSubmit)}
+      ></FormProvider>
+    </>
+  );
+};
 
 export default Loginform;
